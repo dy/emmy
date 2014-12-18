fb52c0b17f0b96ebe16fe550b2b050d4f2769721
Dmitry <df.creative@gmail.com>
2014-12-18 16:52:43 +0300

    Add not wrapper

---
+++ not.js
@@ -0,0 +1,38 @@
+/**
+ * @module  emmy/not
+ */
+
+module.exports = not;
+
+var on = require('./on');
+var closest = typeof document !== 'undefined' ? require('query-relative/closest') : null;
+
+
+/**
+ * Bind listener to a target
+ * listening for all events from it’s children matching selector
+ *
+ * @param {string} selector A selector to match against
+ *
+ * @return {function} A callback
+ */
+function not(target, evt, fn, selector){
+	return on(target, evt, not.wrap(target, evt, fn, selector));
+}
+
+
+/** Return wrapper calling fn in case if selector passes */
+not.wrap = function(target, evt, fn, selector){
+	//ignore non-DOM
+	if (!closest) return;
+
+	return on.wrap(target, evt, fn, function(e){
+		var el = e.target;
+
+		//If source element or anything in-between it and delegate element matches passed selector - ignore that event
+
+		var res = closest(el, selector);
+		if (res && target.contains(res)) return false;
+		return true;
+	});
+};
